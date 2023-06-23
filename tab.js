6f5d5b8ecbc42dc8bd39ee02b21231fe7147f110
조재범 <n23993973@gmail.com>
2023-06-23 19:34:25 +0900

    data binding

--- tab.js
+++ tab.js
@@ -2,26 +2,26 @@ var tball = $('.tab-button');
 var tcall = $('.tab-content');
 
 // 반복문 변수 let
-// for(let i = 0; i< tball.length; i++){
-//   tball.eq(i).on('click',function(){
-//     탭열기(i);
-//   })
-// }
+for(let i = 0; i< tball.length; i++){
+  tball.eq(i).on('click',function(){
+    탭열기(i);
+  })
+}
 
-$('.list').click(function(e){
-  if(e.target == tball[e.target.dataset.id]){
-    탭열기(e.target.dataset.id);
-  }
-  if(e.target == tball[e.target.dataset.id]){
-    탭열기(e.target.dataset.id);
-  }
+// $('.list').click(function(e){
+//   if(e.target == tball[e.target.dataset.id]){
+//     탭열기(e.target.dataset.id);
+//   }
+//   if(e.target == tball[e.target.dataset.id]){
+//     탭열기(e.target.dataset.id);
+//   }
 
-  if(e.target == tball[e.target.dataset.id]){
-    탭열기(e.target.dataset.id);
-  }
+//   if(e.target == tball[e.target.dataset.id]){
+//     탭열기(e.target.dataset.id);
+//   }
 
 
-})
+// })
 
 
 function 탭열기(변수){
